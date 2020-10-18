2284e38a3f7c5bcdd3d0a0a23b85b82a83ebbcc1
tesujiro <tesujiro@gmail.com>
2020-10-18 09:22:56 +0900

    add golang map bench

---
+++ map_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func benchGolangMap_Get(b *testing.B) {
+	db := make(map[string][]byte, b.N)
+
+	set := func(k, v []byte) error {
+		db[string(k)] = v
+		return nil
+	}
+	get := func(k []byte) ([]byte, error) {
+		value, ok := db[string(k)]
+		if !ok {
+			return value, fmt.Errorf("map for %s not found", k)
+		}
+		return value, nil
+
+	}
+
+	benchmarkGet(b, set, get, nil)
+}
+
+func BenchmarkGet_GolangMap(b *testing.B) {
+	b.Run("Memory", func(b *testing.B) {
+		benchGolangMap_Get(b)
+	})
+}
