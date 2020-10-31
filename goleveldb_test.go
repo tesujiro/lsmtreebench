a59895311424336cfca01f2bb6d29b6d4db9e21f
tesujiro <tesujiro@gmail.com>
2020-10-31 19:46:34 +0900

    add range bench for golebeldb

--- goleveldb_test.go
+++ goleveldb_test.go
@@ -6,7 +6,9 @@ import (
 
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/filter"
+	"github.com/syndtr/goleveldb/leveldb/iterator"
 	"github.com/syndtr/goleveldb/leveldb/opt"
+	"github.com/syndtr/goleveldb/leveldb/util"
 )
 
 func benchGoLevelDB_Get(b *testing.B, options *opt.Options) {
@@ -41,5 +43,28 @@ func BenchmarkGet_syndtrGoLevelDB(b *testing.B) {
 			Filter: filter.NewBloomFilter(10),
 		}
 		benchGoLevelDB_Get(b, o)
+	})
+}
+
+func benchGoLevelDB_Range(b *testing.B, options *opt.Options) {
+	db, err := leveldb.OpenFile("testdata-goleveldb", options)
+	if err != nil {
+		fmt.Printf("Open failed: %v", err)
+	}
+	defer db.Close()
+
+	set := func(k, v []byte) error {
+		return db.Put(k, v, nil)
+	}
+	newIterator := func(start, end []byte) iterator.Iterator {
+		return db.NewIterator(&util.Range{Start: []byte(start), Limit: []byte(end)}, nil)
+	}
+
+	benchmarkRange(b, set, nil, newIterator)
+}
+
+func BenchmarkRange_syndtrGoLevelDB(b *testing.B) {
+	b.Run("SSD(Bloom:No)", func(b *testing.B) {
+		benchGoLevelDB_Range(b, nil)
 	})
 }
--- lsmtree_test.go
+++ lsmtree_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+
+	"github.com/syndtr/goleveldb/leveldb/iterator"
 )
 
 func benchmarkGet(b *testing.B, set func([]byte, []byte) error, get func([]byte) ([]byte, error), commit func() error) {
@@ -46,5 +48,57 @@ func benchmarkGet(b *testing.B, set func([]byte, []byte) error, get func([]byte)
 		}
 		_ = g
 	}
+	b.StopTimer()
+}
+
+func benchmarkRange(b *testing.B, set func([]byte, []byte) error, commit func() error, newIterator func(start, end []byte) iterator.Iterator) {
+	//b.ResetTimer()
+	b.StopTimer()
+	//fmt.Printf("b.N:%v\n", b.N)
+	size := b.N
+	keys := make([]string, size)
+	for i := 0; i < b.N; i++ {
+		key := fmt.Sprintf("key%.8d", i)
+		val := fmt.Sprintf("val%.8d", i)
+		keys[i] = key
+
+		err := set([]byte(key), []byte(val))
+		if err != nil {
+			fmt.Printf("Set(%q): %v\n", key, err)
+			return
+		}
+	}
+	rand.Shuffle(len(keys), func(i, j int) {
+		keys[i], keys[j] = keys[j], keys[i]
+	})
+
+	if commit != nil {
+		err := commit()
+		if err != nil {
+			fmt.Printf("Commit: %v\n", err)
+			return
+		}
+	}
+
+	//b.ResetTimer()
+	b.StartTimer()
+
+	key_start := []byte(fmt.Sprintf("key%.8d", 0))
+	key_end := []byte(fmt.Sprintf("key%.8d", b.N))
+	iter := newIterator(key_start, key_end)
+	for iter.Next() {
+		// Use key/value.
+		k := iter.Key()
+		v := iter.Value()
+		_, _ = k, v
+		//fmt.Printf("key=%s\tvalue=%s\n", k, v)
+	}
+	iter.Release()
+	err := iter.Error()
+	if err != nil {
+		fmt.Printf("Release: %v\n", err)
+		return
+	}
+
 	b.StopTimer()
 }
